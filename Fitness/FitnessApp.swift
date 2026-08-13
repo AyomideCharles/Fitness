7187e198ce89096f017b56218b1a5c321409b4f1
AyomideCharles <dettyboy@icloud.com>
2026-08-13 19:41:26 +0100

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Charles on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
